f56a544e2d2b2e88913a0d7753f234be48b4c52f
ssdtsk <susudafamyy@gmail.com>
2023-05-26 22:08:25 +0900

    user.rbのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ devise :database_authenticatable, :registerable,
        has_many :orders
 
        validates :nickname, presence: true
-       validates :email, presence: true, uniqueness: true
+       validates :email, presence: true, uniqueness: { case_sensitive: true }
        validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/}
        validates :password_confirmation, presence: true
        validates :password, confirmation: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
-    email                 { Faker::Internet.free_email }
+    email                 { Faker::Internet.email }
     password              { 'z5' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     first_name            { Gimei.name.first.kanji }
