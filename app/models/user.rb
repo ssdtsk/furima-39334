7989a73a614189d1f2f5edf9c83128db86bcfe41
ssdtsk <susudafamyy@gmail.com>
2023-05-26 12:36:27 +0900

    名前入力オプション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,10 @@ devise :database_authenticatable, :registerable,
        validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: "は英字と数字の両方を含めて設定してください" }
        validates :password_confirmation, presence: true
        validates :password, confirmation: true
-       validates :first_name, presence: true
-       validates :last_name, presence: true
-       validates :last_name_kana, presence: true
-       validates :first_name_kana, presence: true
+       validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください" }
+       validates :last_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください" }
+       validates :last_name_kana, presence: true,format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+       validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
        validates :birthday, presence: true
   
   
