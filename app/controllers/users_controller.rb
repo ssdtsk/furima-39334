016aa6d7249d0a96e9f848faeaee69dfd98e385c
ssdtsk <susudafamyy@gmail.com>
2023-06-03 16:20:53 +0900

    ユーザーコントローラー内削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,2 @@
 class UsersController < ApplicationController
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      @user = User.new(params.require(:user).permit(:nickname, :email, :encrypted_password, :last_name, :first_name, :first_name_kana,:last_name_kana,:birthday,))
-    else
-      render 'new'
-    end
-
-  
-    def show
-      @user = User.find(params[:id])
-    end
-  end
 end
